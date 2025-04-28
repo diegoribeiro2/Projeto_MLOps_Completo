c58c813db373433eb4aed5a2ab87d24f9418d5db
Diego Ribeiro <diegosribeiro@gmail.com>
2025-04-28 17:00:26 -0300

    atualizacao

--- airflow.tf
+++ airflow.tf
@@ -85,7 +85,8 @@ resource "aws_instance" "airflow-server" {
     # copiar os dags do bucket para a pasta de dags
     aws s3 cp s3://${aws_s3_bucket.airflow-dags.bucket} /home/ec2-user/airflow/dags/ --recursive
 
-    nohup airflow standalone --port 8080 > /home/ec2-user/airflow/standalone.log 2>&1 &
+    airflow webserver -p 8080 -D
+    airflow scheduler
 
     EOL
   EOF
